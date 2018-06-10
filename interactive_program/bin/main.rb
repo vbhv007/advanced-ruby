53517955c865e8f5709c20b182687725e82a978b
vbhv007 <bhardwajvbhv@gmail.com>
2018-06-10 18:48:05 +0530

    interactive_program

--- interactive_program/bin/main.rb
+++ interactive_program/bin/main.rb
@@ -2,17 +2,16 @@ require '../lib/interactive.rb'
 ob = Interactive.new
 puts 'Enter the code or q for quit'
 binding_obj = ob.bind_this
-print '>>'
-input = gets
 code = ''
 
-until input =~ /^q$/
+loop do
+  print '>>'
+  input = gets
+  break if input =~ /^q$/
   if input.eql?("\n")
     puts ob.eval_this(code, binding_obj)
     code = ''
   else
     code += input
   end
-  print '>>'
-  input = gets
 end
