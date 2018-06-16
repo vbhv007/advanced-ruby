12f5738525f59a339d7dfa5c47cb7351c1e5551d
vbhv007 <bhardwajvbhv@gmail.com>
2018-06-16 10:40:21 +0530

    interactive_program

--- interactive_program/bin/main.rb
+++ interactive_program/bin/main.rb
@@ -1,17 +1,5 @@
 require '../lib/interactive.rb'
 ob = Interactive.new
 puts 'Enter the code or q for quit'
-binding_obj = ob.bind_this
-code = ''
-
-loop do
-  print '>>'
-  input = gets
-  break if input =~ /^q$/
-  if input.eql?("\n")
-    puts ob.eval_this(code, binding_obj)
-    code = ''
-  else
-    code += input
-  end
-end
+bind_ob = ob.bind_this
+ob.get_code('', bind_ob)
--- interactive_program/lib/interactive.rb
+++ interactive_program/lib/interactive.rb
@@ -7,4 +7,17 @@ class Interactive
   def eval_this(code, binding_obj)
     instance_eval(code, binding_obj.to_s)
   end
+
+  def get_code(code, bind_ob)
+    print '>>'
+    input = gets
+    exit if input =~ /^q$/
+    if input.eql?("\n")
+      puts eval_this(code, bind_ob)
+      code = ''
+    else
+      code += input
+    end
+    get_code(code, bind_ob)
+  end
 end
